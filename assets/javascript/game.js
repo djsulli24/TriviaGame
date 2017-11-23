478e0029cb45eb2da48581cc15c7840ca946671f
djsulli24 <djsulli24@gmail.com>
2017-11-22 22:32:21 -0600

    Basic elements of the game are complete. There is a countdown timer for each question. The answer choices for each question are added on the page in a random order, but they are designated as "wrong" and "correct" via element ID. When the user clicks an answer, they are given an alert saying whether the answer was correct or incorrect. Correct and incorrect answers are tallied. The game cycles through 5 questions, then ends.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,32 +16,57 @@
 $(document).ready(function() {
 
     var trivia = {
+        wins: 0,
+        losses: 0,
         completedQuestions: 0,
-        gameQuestions: {
-            0: "Question 1",
-            1: "Question 2",
-            2: "Question 3",
-            3: "Question 4",
-            4: "Question 5",
-        },
+        intervalId: 0,
+        gameQuestions: [
+            {
+                question: "What was the average life expectancy of white males born in the U.S. just before the Civil War?",
+                incorrectAnswers: ["50 Years", "60 Years", "30 Years"],
+                correctAnswer: "40 Years",
+                answered: false
+            },
+            {
+                question: "Question 2?",
+                incorrectAnswers: ["Answer 1", "Answer 2", "Answer 3"],
+                correctAnswer: "Answer 4",
+                answered: false
+            },
+            {
+                question: "Question 3?",
+                incorrectAnswers: ["Answer 1", "Answer 2", "Answer 3"],
+                correctAnswer: "Answer 4",
+                answered: false
+            },
+            {
+                question: "Question 4?",
+                incorrectAnswers: ["Answer 1", "Answer 2", "Answer 3"],
+                correctAnswer: "Answer 4",
+                answered: false
+            },
+            {
+                question: "Question 5?",
+                incorrectAnswers: ["Answer 1", "Answer 2", "Answer 3"],
+                correctAnswer: "Answer 4",
+                answered: false
+            },
+        ],
         timer: function() {
-            let count = 10;
-            var timerInterval = setInterval(function() {
+            let count = 9;
+            this.intervalId = setInterval(function() {
                 if (count > -1) {
                     $("#timer").text(count);
                     count--;
                 }
                 else {
-                    clearInterval(timerInterval);
-                    trivia.addQuestion();
+                    clearInterval(trivia.intervalId);
+                    trivia.clickedAnswer("noAnswer");
                 }
             }, 1000);
         },
         startGame: function() {
             $("#question").html("<button id='startgame'>Begin</button>");
-        },
-        nextQuestion: function() {
-            this.addQuestion();
         },
         addQuestion: function() {
             if (this.completedQuestions < 5) {
@@ -49,9 +74,9 @@ $(document).ready(function() {
             // hasn't started. Makes it appear synchronous. 
             $("#timer").text("10");
             this.timer();
-            var number = this.completedQuestions.toString();
             $("#question").empty();
-            $("#question").text(this.gameQuestions[number]);
+            $("#question").text(this.gameQuestions[this.completedQuestions].question);
+            this.randomizeAnswers(this.completedQuestions);
             this.completedQuestions++;
             }
             else {
@@ -61,12 +86,61 @@ $(document).ready(function() {
         },
         finishGame: function() {
             alert("Game is over");
+        },
+        clickedAnswer: function(userAnswer) {
+            clearInterval(this.intervalId);
+            if (userAnswer === "wrong") {
+                alert("wrong answer");
+                trivia.losses++;
+            }
+            else if (userAnswer === "correct") {
+                alert("correct");
+                trivia.wins++;
+            }
+            else {
+                alert("you didn't pick an answer");
+                trivia.losses++;
+            }
+            setTimeout(function() {trivia.addQuestion()}, 5000);
+        },
+        // This function returns an array with 0, 1, 2, 3 in a random order.
+        // This is used to randomize the order of the answers.
+        randomFour: function() {
+            var randomFour = [];
+            while (randomFour.length < 4) {
+                let number = Math.floor(Math.random()*4);
+                if (randomFour.indexOf(number) === -1) {
+                    randomFour.push(number);
+                }
+            }
+            return randomFour;
+        },
+        randomizeAnswers: function(questionNumber) {
+            let array = this.randomFour();
+            $("#answers").html("<ol><li></li><li></li><li></li><li></li></ol>");
+            for (var i = 0; i<3; i++) {
+                $("ol").children().eq(array[i]).text(trivia.gameQuestions[questionNumber].incorrectAnswers[i]);
+                $("ol").children().eq(array[i]).attr("id", "wrong");
+            }
+            $("ol").children().eq(array[3]).text(trivia.gameQuestions[questionNumber].correctAnswer);
+            $("ol").children().eq(array[3]).attr("id", "correct");
         }
     };
 
     // Function to run on pageload
     trivia.startGame();
 
+    //------------CLICK EVENTS------------
+
+    // The "start" button for the game
     $("#startgame").click(function() {trivia.addQuestion()});
+
+    // Clicking a wrong answer
+    $("#answers").on("click", "#wrong", function() {trivia.clickedAnswer("wrong")});
+
+    // Clicking the correct answer
+    $("#answers").on("click", "#correct", function() {trivia.clickedAnswer("correct")});
+    
+
 
 });
